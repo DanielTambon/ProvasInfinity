f84423029b0a0760a171a4e34de9a564cb641ae5
DanielTambon <danieltambon74@gmail.com>
2024-09-11 17:19:13 -0300

    Prova2.py

--- Prova2.py
+++ Prova2.py
@@ -1,7 +1,7 @@
 #Velocidade do usuário
 velocidade = int(input('Qual é a velocidade atual do carro? '))
 
-#Anlálise de multa
+#Análise de multa
 if velocidade > 80:
     print('\033[31mMULTADO! Você excedeu o limite permitido que é de 80Km/h\033[m')
     print(f'\033[31mVocê deve pagar uma multa de R$\033[m \033[31m{(velocidade - 80)*20}\033[m')
