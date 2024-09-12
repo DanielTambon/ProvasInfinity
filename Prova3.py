40498120d5c78f94c1e8930f81828b4960ed7d00
DanielTambon <danieltambon74@gmail.com>
2024-09-12 15:45:51 -0300

    Add files via upload

---
+++ Prova3.py
@@ -0,0 +1,18 @@
+#Criando o somatório e a lista de números
+soma = 0
+lista = []
+
+#Geração do loop de digitação dos números
+while True:
+    num = int(input('\033[33mDigite um número: \033[m'))
+    soma += num
+    if num != 0:
+        lista.append(num) 
+    if num == 0:
+        break
+    
+#Variável para fazer o cálculo da média aritimética
+media = soma / len(lista)
+
+#Imprimindo os resultados da soma e da média
+print(f'\033[31mVocê digitou {len(lista)} números e a soma entre eles foi {soma}, enquanto a média aritimética entre eles deu {media:.2f}, com duas casas decimais.\033[m]')
