abbb0032d6609b646c941baec2b6181086d26c2d
DanielTambon <danieltambon74@gmail.com>
2024-09-14 01:45:58 -0300

    Add files via upload

---
+++ ProvaWhile.py
@@ -0,0 +1,22 @@
+# Importa uma biblioteca que escolhe aleatoriamente um número
+import random
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Número que o jogador deve adivinhar
+numero_correto = random.choice(lista)
+# Número máximo de tentativas
+tentativas_maximas = 3
+# Inicializa o número de tentativas
+tentativas = 0
+
+while tentativas < tentativas_maximas:
+    # Solicita ao jogador que adivinhe o número
+    usuario_escolha = int(input("Adivinhe o número (entre 1 e 10): "))
+    tentativas += 1
+    
+    if usuario_escolha == numero_correto:
+        print("Parabéns! Você acertou o número!")
+        # Encerra o loop se o número estiver correto
+        break
+else:
+    # Executado se o loop terminar sem o comando 'break'
+    print(f'Que pena! Você esgotou suas tentativas. O número correto era {numero_correto}! Mas não desista, tente novamente!')
