7c0b28743b637815654e8d7721f94cd3db677899
DanielTambon <danieltambon74@gmail.com>
2024-09-14 01:02:17 -0300

    Add files via upload

---
+++ PovaAlgoritimos.py
@@ -0,0 +1,9 @@
+# Definição da base e altura do retângulo
+base = float(input('Digite a base do retangulo: '))
+altura = float(input('Digite o valor da altura do retangulo: '))
+
+# Cálculo da área do retângulo 
+area = base * altura
+
+# Imrprimindo o resultado
+print(f'A área do retângulo especificado é {area} u.m.!')
