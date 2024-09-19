9f63f6c4bded2b3c48fd2505a6926ebb6c01b361
DanielTambon <danieltambon74@gmail.com>
2024-09-19 03:32:40 -0300

    ProvaListaTuplas.py

--- ProvaListaTuplas.py
+++ ProvaListaTuplas.py
@@ -1,3 +1,8 @@
+# Criação de uma lista com seis frutas
 lista_de_frutas = ['Maçã', 'Banana', 'Morango', 'Abacaxi', 'Laranja', 'Uva']
+
+# Conveção de uma lista em uma tupla
 tupla_de_frutas = tuple(lista_de_frutas)
+
+# Impressão do conteúdo da tupla
 print(tupla_de_frutas)
